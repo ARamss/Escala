369a10672a3ec9f95b7bb9a1b0474ccf8a50ed68
Alberto Ramos <albertoramos@Albertos-Mac-mini.local>
2022-11-06 08:49:16 -0600

    [rams][server]Backend setup

---
+++ server/app.js
@@ -0,0 +1,38 @@
+const { MDB } = require("./config/keys");
+const { PORT } = require("./config");
+const { error, success } = require("consola");
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require("morgan");
+const path = require('path');
+const cors = require('cors');
+const app = express();
+const bodyParser = require("body-parser");
+
+//profix
+const base_api = "/api/v1";
+
+app.use(cors());
+app.use(morgan("tiny"));
+
+//connection status
+mongoose.connect(MDB, (err, res) => {
+  if (err) return error(err);
+  success("Data Base online");
+});
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+//generate public directory
+app.use("/", express.static(path.resolve(__dirname, "../public")));
+
+//global routes config
+// app.use(base_api, require("./routes/index"));
+
+app.listen(process.env.PORT || 5050, () => {
+  success("Server is running on port: ", PORT);
+});
---
+++ server/config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MDB: "mongodb+srv://justRamss:31H7dR0nmCqzURi9@cluster0.mdr2lic.mongodb.net/testDB?retryWrites=true&w=majority",
+  JWT_SECRET: "9PS7cbePS6kR0Rvgtr",
+};
