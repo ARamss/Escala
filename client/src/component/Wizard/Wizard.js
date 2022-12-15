c917192ea962bedba5e437a91a4e0e12b0e34f3a
Alberto Ramos <albertoramos@Albertos-Mac-mini.local>
2022-12-15 17:56:38 -0600

    [rams][client] Wizard setup

--- client/src/component/Wizard/Wizard.js
+++ client/src/component/Wizard/Wizard.js
@@ -1,51 +1,69 @@
 import React from "react";
 
-const Wizard = ({ children }) => {
-    const [activePageIndex, setActivePageIndex] = React.useState(0);
-    const pages = React.Children.toArray(children);
-    const currentPage = pages[activePageIndex];
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/bootstrap-datepicker.css';
+import './assets/css/fontawesome-all.css';
+import './assets/css/animate.min.css';
 
-    const goNextPage = () => {
-      setActivePageIndex(index => index + 1);
-    };
+//main css
+import './assets/scss/style.scss';
 
-    const goPrevPage = () => {
-      setActivePageIndex(index => index - 1);
-    };
+import 'bootstrap';
 
-    const ButtonPrev = () =>
-      activePageIndex > 0 ? (
-        <button
-          type="button"
-          onClick={goPrevPage}
-          className="btn btn-primary btn-block btn-sm"
-        >
-          Atras
-        </button>
-      ) : null;
+import './assets/js/bootstrap-datepicker.min.js';
+import './assets/js/jquery.validate.min.js';
+import './assets/js/main.js';
+//
+// module.hot.accept();
 
-    const ButtonNext = () =>
-      activePageIndex < pages.length - 1 ? (
-        <button
-          type="button"
-          onClick={goNextPage}
-          className="btn btn-primary btn-block btn-sm"
-        >
-          Siguiente
-        </button>
-      ) : null;
+import Step1 from './step/step-1';
+import Step2 from './step/step-2';
+import Step3 from './step/step-3';
+import Step4 from './step/step-4';
+import Step5 from './step/step-5';
 
+
+class Wizard extends React.Component {
+render() {
     return (
-      <React.Fragment>
-      <div className="wizard">
-        <div className="wizard__content">{currentPage}</div>
-          <div className="wizard__buttons">
-            <ButtonPrev />
-            <ButtonNext />
+      <div className="wrapper">
+          <div className="steps-area steps-area-fixed">
+              <div className="image-holder">
+                  <img src={require("./assets/img/side-img.jpg")} alt="" />
+              </div>
+              <div className="steps clearfix">
+                  <ul className="tablist multisteps-form__progress">
+                      <li className="multisteps-form__progress-btn js-active current">
+                          <span>1</span>
+                      </li>
+                      <li className="multisteps-form__progress-btn">
+                          <span>2</span>
+                      </li>
+                      <li className="multisteps-form__progress-btn">
+                          <span>3</span>
+                      </li>
+                      <li className="multisteps-form__progress-btn">
+                          <span>4</span>
+                      </li>
+                      <li className="multisteps-form__progress-btn last">
+                          <span>5</span>
+                      </li>
+                  </ul>
+              </div>
           </div>
-       </div>
-      </React.Fragment>
-    );
-}
+          <form className="multisteps-form__form" action="#" id="wizard" method="POST">
+              <div className="form-area position-relative">
+                  <Step1 />
+                  <Step2 />
+                  <Step3 />
+                  <Step4 />
+                  <Step5 />
+              </div>
+          </form>
+      </div>
+    )
+  }
+};
+
 
 export default Wizard;
